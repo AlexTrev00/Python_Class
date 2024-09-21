a15c6ee9907de59cf22ba285d302a20de5a4a24b
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-09-20 21:28:31 -0600

    Update funciones.py

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -8,3 +8,8 @@ def imprime_lista(lista):
 milista=["hola","adios","bye","trece"]
 imprime_lista(milista) 
 
+#explicacion: en el bloque if isinstance(lista,list)
+# isinstance es una funcion propia de python y veirifca el tipo de un objeto con la ayuda de dos argumentos en este caso 
+#(lista,list) el primer argumento (lista) comprueba el tipo de dato u objeto que quieres comprobar y el segundo es el tipo 
+# de dato que estas comprobando, en este caso con isinstance(lista,list) comprueba si la lista es realmente un lista. 
+
