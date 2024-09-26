e1893070a3c0f97b19372454c97cddab537110b1
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-09-26 15:01:01 -0600

    Add files via upload

---
+++ funciones_internas_python.py
@@ -0,0 +1,9 @@
+'''funciones internas 
+python porporcionar un numero importante de funciones internas, que pueden ser usadas sin necesidad de tener que definirlas previamente.
+
+las funciones max y min nos daran respectivamente el valor mayor y menor de una lista. 
+'''
+	
+print(max("¡hola mundo!"))
+print(min("¡hola mundo!"))
+ 
